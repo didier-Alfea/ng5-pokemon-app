e7546d52ce633e466e1614e915253149b0ee0d0f
didier-Alfea <Didier.CLIN@alfea-consulting.com>
2019-01-14 14:21:26 +0100

    Ajout Data in memory pour simuler API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { PokemonsModule } from './pokemons/pokemons.module';
 
+// Import du module http
+import { HttpClientModule } from '@angular/common/http';
+
+//
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }from './in-memory-data.service';
+
 import { AppComponent }           from './app.component';
 import { PageNotFoundComponent } from './page-not-found.component';
+
 
 @NgModule({
   imports:      [
     // Ordre est important
     BrowserModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation: false}),
     PokemonsModule,
     AppRoutingModule
    ],
