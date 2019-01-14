c67f6ee5d39b77cdc8f20c25273d51ebf1a798a7
didier-Alfea <Didier.CLIN@alfea-consulting.com>
2019-01-14 14:48:40 +0100

    mise en place API pour la liste des pokemeons

--- src/app/pokemons/pokemons.service.ts
+++ src/app/pokemons/pokemons.service.ts
@@ -2,12 +2,36 @@ import { Injectable } from '@angular/core';
 import { Pokemon } from './pokemon';
 import { POKEMONS } from './mock-pokemons';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { catchError, map, tap } from 'rxjs/operators';
+import { of } from "rxjs/observable/of";
+
 @Injectable()
 export class PokemonsService {
 
+  constructor(private http: HttpClient) {}
+
+  private pokemonsUrl = 'api/pokemons';
+
+  private log(log: string) {
+    console.info(log);
+  }
+
+  private handleError<T>(operation='operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error);
+      console.log(`${operation} failed: ${error.message}`);
+
+    return of(result as T);
+    }
+  }
   //retourne tous les Pokemons
-  getPokemons(): Pokemon[] {
-    return POKEMONS;
+  getPokemons(): Observable<Pokemon[]> {
+    return this.http.get<Pokemon[]>(this.pokemonsUrl).pipe(
+      tap(_ => this.log(`fetched pokemons`)),
+      catchError(this.handleError(`getPokemons`, []))
+    );
   }
 
   // retourne le pokemon avec l'identifiant passé en parametre
--- src/dist/app/pokemons/pokemons.service.js
+++ src/dist/app/pokemons/pokemons.service.js
@@ -5,15 +5,34 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
-var mock_pokemons_1 = require("./mock-pokemons");
+var http_1 = require("@angular/common/http");
+var operators_1 = require("rxjs/operators");
+var of_1 = require("rxjs/observable/of");
 var PokemonsService = (function () {
-    function PokemonsService() {
+    function PokemonsService(http) {
+        this.http = http;
+        this.pokemonsUrl = 'api/pokemons';
     }
+    PokemonsService.prototype.log = function (log) {
+        console.info(log);
+    };
+    PokemonsService.prototype.handleError = function (operation, result) {
+        if (operation === void 0) { operation = 'operation'; }
+        return function (error) {
+            console.log(error);
+            console.log(operation + " failed: " + error.message);
+            return of_1.of(result);
+        };
+    };
     //retourne tous les Pokemons
     PokemonsService.prototype.getPokemons = function () {
-        return mock_pokemons_1.POKEMONS;
+        var _this = this;
+        return this.http.get(this.pokemonsUrl).pipe(operators_1.tap(function (_) { return _this.log("fetched pokemons"); }), operators_1.catchError(this.handleError("getPokemons", [])));
     };
     // retourne le pokemon avec l'identifiant passé en parametre
     PokemonsService.prototype.getPokemon = function (id) {
@@ -28,7 +47,8 @@ var PokemonsService = (function () {
         return ['Plante', 'Feu', 'Eau', 'Insecte', 'Normal', 'Electrik', 'Poison', 'Fée', 'Vol'];
     };
     PokemonsService = __decorate([
-        core_1.Injectable()
+        core_1.Injectable(),
+        __metadata("design:paramtypes", [http_1.HttpClient])
     ], PokemonsService);
     return PokemonsService;
 }());
